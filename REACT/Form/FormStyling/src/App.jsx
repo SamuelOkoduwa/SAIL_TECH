0b4109e47aef37ea05c6158e7940e4724290b401
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-03-22 16:29:12 +0100

    React Form

---
+++ REACT/Form/FormStyling/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState({
+
+  })
+
+  const handlechange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+
+    
+  }
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
