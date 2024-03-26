757235d19ad337672a8fe4e257a59fe30a3e78cd
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-03-27 00:05:05 +0100

    Todolist

--- Assessment_React/TodoList/TodoList/src/components/Todo.jsx
+++ Assessment_React/TodoList/TodoList/src/components/Todo.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 
+let storage = [];
+
+
 function TodoList(){
     return(
         <div>
-            
+           <input type="text" /> 
+           <button>Submit</button>
         </div>
     )
 }
+
+export default TodoList
